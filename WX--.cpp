c1bfa7952200d677620a55851f107276b2a46a6c
saltq144 <91617568+saltq144@users.noreply.github.com>
2021-10-02 17:42:21 +0300

    Better readibility, bug fix
    Removed goto statements
    Started using a clean for loop
    Fixed error message showing when an argument was the path of the file

--- WX--.cpp
+++ WX--.cpp
@@ -3,48 +3,24 @@
 #include "inc.h"
 
 
-//Switch out the "//#pragma"s for "#pragma"s if you are using  GCC / clang like this:
-//If you are using vs then dont, if you are using neither i have no words for you
-/*
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-//#pragma warning( push )
-//#pragma warning( disable : 4100 )
 int main(int argc, char* argv[]) {
-//#pragma warning( pop )
-#pragma GCC diagnostic pop
-*/
-
-//#pragma GCC diagnostic push
-//#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma warning( push )
-#pragma warning( disable : 4100 )
-int main(int argc, char* argv[]) {
-#pragma warning( pop )
-//#pragma GCC diagnostic pop
 	
-	int boolean_flags_argvIndex = 1;
 	bool boolean_flags_forceHelp = false;
 	bool boolean_flags_help = false;
-	
-boolean_flags_whileStart:
-	//string(argv[1]) == "file"
-	if (argv[boolean_flags_argvIndex] == NULL) {
-		goto boolean_flags_whileEnd;
-	} else if (std::string(argv[boolean_flags_argvIndex]) == "-h") {
-		boolean_flags_help = true;
-	} else if (std::string(argv[boolean_flags_argvIndex]) == argv[0]) {
-		std::cout << "\n*\n**\n***\n****\n";
-		std::cout << "This message should not be displayed under normal circumstances          \n";
-		std::cout << "Please contact support@creationship27.com and report this error          \n";
-		std::cout << "*\n**\n***\n****";
-	} else {
-		boolean_flags_forceHelp = true;
+	for (int i = 1; i < argc; i++) {
+		//Lack of switch statement because std::string(argv[i]) is not an integral or
+		//enumerated value.
+		if (std::string(argv[i]) == "-h") {
+			boolean_flags_help = true;
+		}
+		else if (std::string(argv[i]) == "-t") {
+			std::cout << "Testing not implemented";
+		}
+		else {
+			boolean_flags_forceHelp = true;
+		}
 	}
-	boolean_flags_argvIndex++;
-	goto boolean_flags_whileStart;
-boolean_flags_whileEnd:
-	if (boolean_flags_forceHelp || boolean_flags_help) {
+	if (boolean_flags_forceHelp || boolean_flags_help || argc == 1) {
 		usage();
 	}
 	USsleep(5000000);
